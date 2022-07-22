9af77c3a8d1a5fb34c3f9e80e34ef1747109d48f
Claudio <costa.pederneschi@gmail.com>
2022-07-22 19:05:59 -0300

    Entrega

--- database.js
+++ database.js
@@ -1,54 +1,66 @@
 const products = [
     {
-      title: "Uva",
+      id:1,
+      name: "Uva",
       price: 8.99,
     },
     {
-      title: "Banana",
+      id:2,
+      name: "Banana",
       price: 5.69,
     },
     {
-      title: "Mamão",
+      id:3,
+      name: "Mamão",
       price: 4.99,
     },
     {
-      title: "Maçã",
+      id:4,
+      name: "Maçã",
       price: 9.2,
     },
     {
-      title: "Coca-Cola",
+      id:5,
+      name: "Coca-Cola",
       price: 8.99,
     },
     {
-      title: "Fanta Uva",
+      id:6,
+      name: "Fanta Uva",
       price: 8.99,
     },
     {
-      title: "Guarana Antartica",
+      id:7,
+      name: "Guarana Antartica",
       price: 8.99,
     },
     {
-      title: "Pepsi",
-      price: 8.99,
-    },
-  
-    {
-      title: "Sabonete",
-      price: 8.99,
-    },
-  
-    {
-      title: "Detergente",
+      id:8,
+      name: "Pepsi",
       price: 8.99,
     },
   
     {
-      title: "Alvejante",
+      id:9,
+      name: "Sabonete",
       price: 8.99,
     },
   
     {
-      title: "Candida",
+      id:10,
+      name: "Detergente",
+      price: 8.99,
+    },
+  
+    {
+      id:11,
+      name: "Alvejante",
+      price: 8.99,
+    },
+  
+    {
+      id:12,
+      name: "Candida",
       price: 8.99,
     },
   ];
