014d947d0b059337f4bf974133d9ea4fdae344c6
Claudio <costa.pederneschi@gmail.com>
2022-07-22 19:01:02 -0300

    Entrega

---
+++ database.js
@@ -0,0 +1,54 @@
+const products = [
+    {
+      title: "Uva",
+      price: 8.99,
+    },
+    {
+      title: "Banana",
+      price: 5.69,
+    },
+    {
+      title: "Mamão",
+      price: 4.99,
+    },
+    {
+      title: "Maçã",
+      price: 9.2,
+    },
+    {
+      title: "Coca-Cola",
+      price: 8.99,
+    },
+    {
+      title: "Fanta Uva",
+      price: 8.99,
+    },
+    {
+      title: "Guarana Antartica",
+      price: 8.99,
+    },
+    {
+      title: "Pepsi",
+      price: 8.99,
+    },
+  
+    {
+      title: "Sabonete",
+      price: 8.99,
+    },
+  
+    {
+      title: "Detergente",
+      price: 8.99,
+    },
+  
+    {
+      title: "Alvejante",
+      price: 8.99,
+    },
+  
+    {
+      title: "Candida",
+      price: 8.99,
+    },
+  ];
--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "",
-        price: 0.00
-    }
-]
+let total = 0
+const corpo = document.getElementsByTagName("body")
+const _main = document.createElement("main")
+_main.className = "maincontent"
+const _container = document.createElement("div")
+_container.className = "containerprincipal"
+const _ulpri = document.createElement("ul")
+_ulpri.className = "ulprincipal"
+const _divtotal = document.createElement("div")
+_divtotal.className = "total"
+const _total = document.createElement("p")
+_total.textContent = "R$"+total
+const _butt = document.createElement("button")
+_butt.textContent = "Finalizar compras"
+corpo[0].appendChild(_main)
+_main.appendChild(_container)
+_container.appendChild(_ulpri)
+for (let index = 0; index < products.length; index++) {
+    const element = products[index];
+    const _item = document.createElement("li")
+    _item.className = "item"
+    const _nome = document.createElement("p")
+    _nome.className = "name"
+    _nome.textContent = element.title
+    const _preco = document.createElement("p")
+    _preco.className = "price"
+    _preco.textContent = "R$"+element.price
+    total = total + (element.price*100)
+    _ulpri.appendChild(_item)
+    _item.appendChild(_nome)
+    _item.appendChild(_preco)
+}
+_total.textContent = "R$"+(total/100)
+_container.appendChild(_divtotal)
+_divtotal.appendChild(_total)
+_divtotal.appendChild(_butt)
